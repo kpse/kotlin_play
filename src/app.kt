528c365ed86941f2db4e2261c7ec7dabd4964abe
Suo Qin <kpsesuo@gmail.com>
2017-05-28 10:41:02 +1000

    map, range, ignore out directory

--- src/app.kt
+++ src/app.kt
@@ -6,8 +6,33 @@ fun main(args: Array<String>) {
     val sum: (Int, Int) -> Int = { x, y -> x + y }
     println("sum 1 + 2 = " + sum(1, 2))
 
-    val double: (Int, Int, (Int, Int) -> Int) -> Int = { x, y, z -> z(x,y) * 2 }
+    val double: (Int, Int, (Int, Int) -> Int) -> Int = { x, y, z -> z(x, y) * 2 }
 
-    println("double sum 3, 4 = " + double(3,4, sum))
+    println("double sum 3, 4 = " + double(3, 4, sum))
 
+    map()
+
+    range()
+
+}
+
+private fun map() {
+    val numbers = arrayOf(1, 2, 3, 4, 5)
+
+    val squareNumbers = numbers.map({ x -> x * x })
+    println("squareNumbers are " + squareNumbers)
+}
+
+private fun range() {
+    val r1 = 1..5
+
+    println("val r1 = 1..5 : " + r1.toList())
+
+    val r2 = 5 downTo 1
+
+    println("val r2 = 5 downTo 1 : " + r2.toList())
+
+    val r3 = 5 downTo 1 step 2
+
+    println("val r3 = 5 downTo 1 step 2 : " + r3.toList())
 }
